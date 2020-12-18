d80ae333775c74f84688a40278a79a62fb3ed123
Nikita Peplov <npeplov@yandex.ru>
2020-12-18 15:08:45 +0300

    mocking.test: separated CalledWith (url) and Server response (ok/not)

--- test-ci/src/js/__tests__/mocking.test.js
+++ test-ci/src/js/__tests__/mocking.test.js
@@ -3,20 +3,25 @@ import fetchData from '../http.js';
 
 jest.mock('../http.js');
 
-test('getLevel of elf80 returns ok', () => {
-  const response = {
+test('getLevel() of elf80 if Server Returns Ok', () => {
+  fetchData.mockReturnValueOnce({
     status: 'ok',
     level: 80,
-  };
-  fetchData.mockReturnValue(response);
-
+  });
   expect(getLevel('elf80')).toBe('Ваш текущий уровень: 80');
-  expect(fetchData).toBeCalledWith('https://server/user/elf80');
 });
 
-test('getLevel returns Service Unavailable', () => {
-  fetchData.mockReturnValue({});
-
+test('getLevel() if Server Unavailable', () => {
+  fetchData.mockReturnValue('{}');
   expect(getLevel('elf90')).toBe('Информация об уровне временно недоступна');
-  expect(fetchData).toBeCalledWith('https://server/user/elf90');
+});
+
+test('getlevel() of elf80 Called With https://server/user/elf80', () => {
+  getLevel('elf80');
+  expect(fetchData).toHaveBeenLastCalledWith('https://server/user/elf80');
+});
+
+test('getlevel() of elf91 Not Called With https://server/user/elf90', () => {
+  getLevel('elf91');
+  expect(fetchData).not.toHaveBeenLastCalledWith('https://server/user/elf90');
 });
