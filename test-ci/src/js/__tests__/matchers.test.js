5f1e9226962168032d10a74c2746e06ade8a65b8
Nikita Peplov <npeplov@yandex.ru>
2020-12-18 12:54:00 +0300

    matchers.test: added toBe test by Buffer.equals

--- test-ci/src/js/__tests__/matchers.test.js
+++ test-ci/src/js/__tests__/matchers.test.js
@@ -1,17 +1,23 @@
 import sortByHealth from '../matchers.js';
 
+const inputArr = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+const expectedArr = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
 test('sortByHealth 10 100 80', () => {
-  expect(sortByHealth(
-    [
-      { name: 'мечник', health: 10 },
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-    ],
-  )).toEqual(
-    [
-      { name: 'маг', health: 100 },
-      { name: 'лучник', health: 80 },
-      { name: 'мечник', health: 10 },
-    ],
-  );
+  expect(sortByHealth(inputArr)).toEqual(expectedArr);
+});
+
+test('sortByHealth: toBe - 10 100 80', () => {
+  const buff1 = Buffer.from(sortByHealth(inputArr));
+  const buff2 = Buffer.from(expectedArr);
+
+  expect(buff1.equals(buff2)).toBe(true);
 });
