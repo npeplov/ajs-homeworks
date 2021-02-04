f6f4fec8165ec53213aa230cc923ae77f0f0e67e
npeplov <npeplov@yandex.ru>
2021-02-04 10:43:25 +0300

    symbols: 3nd task - origin

---
+++ symbols-iterators-generators/generators/src/js/app.js
@@ -0,0 +1,10 @@
+function canIterate(obj) {
+    if (!obj) return false;
+    else return !!(obj[Symbol.iterator]);
+}
+
+console.log(canIterate(new Map())); // true
+console.log(canIterate(new Set())); // true
+console.log(canIterate(null)); // false
+console.log(canIterate(10)); // false
+console.log(canIterate("Netology")); // true
